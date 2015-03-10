73f3ec22d62b1b83257b6955e69101ce8cf9219c
Stephanie A. Higa <sahiga@gmail.com>
2015-03-09 23:47:23 -0700

    More writers; click transitions

--- writers.js
+++ writers.js
@@ -258,6 +258,7 @@ var writers = {
       {'source': 0, 'target': 4},
       {'source': 1, 'target': 4},
       {'source': 2, 'target': 4},
+      {'source': 3, 'target': 4},
       {'source': 5, 'target': 4},
       {'source': 6, 'target': 4}
     ]
@@ -359,12 +360,12 @@ var writers = {
       {'nodeName': 'François Rabelais', 'group': 1},
       {'nodeName': 'Miguel de Cervantes', 'group': 1},
       {'nodeName': 'Laurence Sterne', 'group': 0},
-      {'nodeName': 'James Joyce', 'group': 1},
-      {'nodeName': 'Virginia Woolf', 'group': 1},
-      {'nodeName': 'Thomas Pynchon', 'group': 1},
-      {'nodeName': 'David Foster Wallace', 'group': 1},
-      {'nodeName': 'Italo Calvino', 'group': 1},
-      {'nodeName': 'Viktor Shklovsky', 'group': 1}
+      {'nodeName': 'James Joyce', 'group': 2},
+      {'nodeName': 'Virginia Woolf', 'group': 2},
+      {'nodeName': 'Thomas Pynchon', 'group': 2},
+      {'nodeName': 'David Foster Wallace', 'group': 2},
+      {'nodeName': 'Italo Calvino', 'group': 2},
+      {'nodeName': 'Viktor Shklovsky', 'group': 2}
     ],
 
     'links': [
@@ -377,5 +378,227 @@ var writers = {
       {'source': 7, 'target': 2},
       {'source': 8, 'target': 2},
     ]
+  },
+
+  'Joseph Conrad': {
+    'nodes': [
+      {'nodeName': 'Joseph Conrad', 'group': 0},
+      {'nodeName': 'D.H. Lawrence', 'group': 2},
+      {'nodeName': 'T.S. Eliot', 'group': 2},
+      {'nodeName': 'F. Scott Fitzgerald', 'group': 2},
+      {'nodeName': 'William Faulkner', 'group': 2},
+      {'nodeName': 'Gerald Basil Edwards', 'group': 2},
+      {'nodeName': 'Ernest Hemingway', 'group': 2},
+      {'nodeName': 'George Orwell', 'group': 2},
+      {'nodeName': 'Graham Greene', 'group': 2},
+      {'nodeName': 'Malcolm Lowry', 'group': 2},
+      {'nodeName': 'William Golding', 'group': 2},
+      {'nodeName': 'William S. Burroughs', 'group': 2},
+      {'nodeName': 'Joseph Heller', 'group': 2},
+      {'nodeName': 'Italo Calvino', 'group': 2},
+      {'nodeName': 'Gabriel García Márquez', 'group': 2},
+      {'nodeName': 'J.G. Ballard', 'group': 2},
+      {'nodeName': 'Chinua Achebe', 'group': 2},
+      {'nodeName': 'John le Carré', 'group': 2},
+      {'nodeName': 'V.S. Naipaul', 'group': 2},
+      {'nodeName': 'Philip Roth', 'group': 2},
+      {'nodeName': 'Hunter S. Thompson', 'group': 2},
+      {'nodeName': 'J.M. Coetzee', 'group': 2},
+      {'nodeName': 'Stephen Donaldson', 'group': 2},
+      {'nodeName': 'Salman Rushdie', 'group': 2},
+      {'nodeName': 'Thomas Pynchon', 'group': 2}
+    ],
+
+    'links': [
+      {'source': 1, 'target': 0},
+      {'source': 2, 'target': 0},
+      {'source': 3, 'target': 0},
+      {'source': 4, 'target': 0},
+      {'source': 5, 'target': 0},
+      {'source': 6, 'target': 0},
+      {'source': 7, 'target': 0},
+      {'source': 8, 'target': 0},
+      {'source': 9, 'target': 0},
+      {'source': 10, 'target': 0},
+      {'source': 11, 'target': 0},
+      {'source': 12, 'target': 0},
+      {'source': 13, 'target': 0},
+      {'source': 14, 'target': 0},
+      {'source': 15, 'target': 0},
+      {'source': 16, 'target': 0},
+      {'source': 17, 'target': 0},
+      {'source': 18, 'target': 0},
+      {'source': 19, 'target': 0},
+      {'source': 20, 'target': 0},
+      {'source': 21, 'target': 0},
+      {'source': 22, 'target': 0},
+      {'source': 23, 'target': 0}
+    ]
+  },
+
+  'William S. Burroughs': {
+    'nodes': [
+      {'nodeName': 'William S. Burroughs', 'group': 0},
+      {'nodeName': 'Peter Ackroyd', 'group': 2},
+      {'nodeName': 'Lester Bangs', 'group': 2},
+      {'nodeName': 'Gilles Deleuze', 'group': 2},
+      {'nodeName': 'J.G. Ballard', 'group': 2},
+      {'nodeName': 'Angela Carter', 'group': 2},
+      {'nodeName': 'Jean Genet', 'group': 2},
+      {'nodeName': 'William Gibson', 'group': 2},
+      {'nodeName': 'Alan Moore', 'group': 2},
+      {'nodeName': 'Kathy Acker', 'group': 2},
+      {'nodeName': 'Ken Kesey', 'group': 2},
+      {'nodeName': 'Carl Weissner', 'group': 2},
+      {'nodeName': 'John Shirley', 'group': 2},
+      {'nodeName': 'Thomas Pynchon', 'group': 2}
+    ],
+
+    'links': [
+      {'source': 1, 'target': 0},
+      {'source': 2, 'target': 0},
+      {'source': 3, 'target': 0},
+      {'source': 4, 'target': 0},
+      {'source': 5, 'target': 0},
+      {'source': 6, 'target': 0},
+      {'source': 7, 'target': 0},
+      {'source': 8, 'target': 0},
+      {'source': 9, 'target': 0},
+      {'source': 10, 'target': 0},
+      {'source': 11, 'target': 0},
+      {'source': 12, 'target': 0},
+      {'source': 13, 'target': 0}
+    ]
+  },
+
+  'James Joyce': {
+    'nodes': [
+      {'nodeName': 'Thomas Aquinas', 'group': 1},
+      {'nodeName': 'Giambattista Vico', 'group': 1},
+      {'nodeName': 'Giordano Bruno', 'group': 1},
+      {'nodeName': 'T.S. Eliot', 'group': 1},
+      {'nodeName': 'James Joyce', 'group': 0},
+      {'nodeName': 'Samuel Beckett', 'group': 2},
+      {'nodeName': 'Seán Ó Ríordáin', 'group': 2},
+      {'nodeName': 'Jorge Luis Borges', 'group': 2},
+      {'nodeName': 'Flann O\'Brien', 'group': 2},
+      {'nodeName': 'Salman Rushdie', 'group': 2},
+      {'nodeName': 'Robert Anton Wilson', 'group': 2},
+      {'nodeName': 'John Updike', 'group': 2},
+      {'nodeName': 'David Lodge', 'group': 2},
+      {'nodeName': 'Joseph Campbell', 'group': 2},
+      {'nodeName': 'Jack Kerouac', 'group': 2},
+      {'nodeName': 'Thomas Pynchon', 'group': 2}
+    ],
+
+    'links': [
+      {'source': 0, 'target': 4},
+      {'source': 1, 'target': 4},
+      {'source': 2, 'target': 4},
+      {'source': 3, 'target': 4},
+      {'source': 5, 'target': 4},
+      {'source': 6, 'target': 4},
+      {'source': 7, 'target': 4},
+      {'source': 8, 'target': 4},
+      {'source': 9, 'target': 4},
+      {'source': 10, 'target': 4},
+      {'source': 11, 'target': 4},
+      {'source': 12, 'target': 4},
+      {'source': 13, 'target': 4},
+      {'source': 14, 'target': 4},
+      {'source': 15, 'target': 4}
+    ]
+  },
+
+  'John Dos Passos': {
+    'nodes': [
+      {'nodeName': 'John Dos Passos', 'group': 0},   
+      {'nodeName': 'Alfred Döblin', 'group': 2},
+      {'nodeName': 'Jean-Paul Sartre', 'group': 2},
+      {'nodeName': 'Mary McCarthy', 'group': 2},
+      {'nodeName': 'Norman Mailer', 'group': 2},
+      {'nodeName': 'John Brunner', 'group': 2},
+      {'nodeName': 'Joe Haldeman', 'group': 2},
+      {'nodeName': 'Thomas Pynchon', 'group': 2}
+    ],
+
+    'links': [
+      {'source': 1, 'target': 0},
+      {'source': 2, 'target': 0},
+      {'source': 3, 'target': 0},
+      {'source': 4, 'target': 0},
+      {'source': 5, 'target': 0},
+      {'source': 6, 'target': 0},
+      {'source': 7, 'target': 0}
+    ]
+  },
+
+  'Jack Kerouac': {
+    'nodes': [
+      {'nodeName': 'Thomas Wolfe', 'group': 1},
+      {'nodeName': 'Gary Snyder', 'group': 1},
+      {'nodeName': 'Neal Cassady', 'group': 1},
+      {'nodeName': 'James Joyce', 'group': 1},
+      {'nodeName': 'Jack Kerouac', 'group': 0},
+      {'nodeName': 'Allen Ginsberg', 'group': 2},
+      {'nodeName': 'Thomas Pynchon', 'group': 2}
+    ],
+
+    'links': [
+      {'source': 0, 'target': 4},
+      {'source': 1, 'target': 4},
+      {'source': 2, 'target': 4},
+      {'source': 3, 'target': 4},
+      {'source': 5, 'target': 4},
+      {'source': 6, 'target': 4}
+    ]
+  },
+
+  'T.S. Eliot': {
+    'nodes': [
+      {'nodeName': 'Omar Khayyam', 'group': 1},
+      {'nodeName': 'Edward Fitzgerald', 'group': 1},
+      {'nodeName': 'Ezra Pound', 'group': 1},
+      {'nodeName': 'Paul Valéry', 'group': 1},
+      {'nodeName': 'Charles Baudelaire', 'group': 1},
+      {'nodeName': 'Dante Alighieri', 'group': 1},
+      {'nodeName': 'T.S. Eliot', 'group': 0},
+      {'nodeName': 'Cynthia Ozick', 'group': 2},
+      {'nodeName': 'Seán Ó Ríordáin', 'group': 2},
+      {'nodeName': 'Eoghan O Tuairisc', 'group': 2},
+      {'nodeName': 'Virginia Woolf', 'group': 2},
+      {'nodeName': 'Hart Crane', 'group': 2},
+      {'nodeName': 'William Gaddis', 'group': 2},
+      {'nodeName': 'Allen Tate', 'group': 2},
+      {'nodeName': 'Ted Hughes', 'group': 2},
+      {'nodeName': 'Geoffrey Hill', 'group': 2},
+      {'nodeName': 'Seamus Heaney', 'group': 2},
+      {'nodeName': 'Russell Kirk', 'group': 2},
+      {'nodeName': 'James Joyce', 'group': 2},
+      {'nodeName': 'Thomas Pynchon', 'group': 2}
+    ],
+
+    'links': [
+      {'source': 0, 'target': 6},
+      {'source': 1, 'target': 6},
+      {'source': 2, 'target': 6},
+      {'source': 3, 'target': 6},
+      {'source': 4, 'target': 6},
+      {'source': 5, 'target': 6},
+      {'source': 7, 'target': 6},
+      {'source': 8, 'target': 6},
+      {'source': 9, 'target': 6},
+      {'source': 10, 'target': 6},
+      {'source': 11, 'target': 6},
+      {'source': 12, 'target': 6},
+      {'source': 13, 'target': 6},
+      {'source': 14, 'target': 6},
+      {'source': 15, 'target': 6},
+      {'source': 16, 'target': 6},
+      {'source': 17, 'target': 6},
+      {'source': 18, 'target': 6},
+      {'source': 19, 'target': 6}
+    ]
+
   }
 };
